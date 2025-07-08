d94a7984c528a583bb94dd8968044402f41446e5
KRIPERSHOK <kalashnikov.nikita2604@gmail.com>
2025-07-08 20:27:07 +0300

    Minor bugfixes

--- src/App.jsx
+++ src/App.jsx
@@ -81,8 +81,18 @@ function Game() {
 
   return (
     <div className="app-container">
-      <div className="score-ui">
-        <p>{score}</p>
+      <div className="ui-container">
+        <div className="score-ui">
+          <p>{score}</p>
+        </div>
+
+        {gameState === 'gameOver' && (
+          <div className="game-over-ui" onClick={placeBlock}>
+            <img src={gameOverImage} alt="Game Over" className="game-over-image" />
+            <p>Ваш счет: {score}</p>
+            <p>Кликните, чтобы начать заново</p>
+          </div>
+        )}
       </div>
       <Canvas shadows camera={{ position: [0, 5, 8], fov: 75 }}>
         <GameCamera towerHeight={towerHeight} />
